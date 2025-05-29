a669c1fae828f742e0b5c1d2832ccf304e587d83
Hubertus Wasmer <hwasmer1@gmail.com>
2025-05-29 16:10:54 +0200

    SetupDone

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,6 +2,7 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin "bootstrap" # @5.3.6
+pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "vue", to: 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.min.js'
+pin_all_from "app/javascript/components", under: "components"
